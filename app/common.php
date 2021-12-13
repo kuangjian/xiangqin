d359553119ffffc44bc26bf1b8e92029cf70b178
ajax <13265679945@163.com>
2021-12-13 09:44:09 +0800

    创建仓库

---
+++ app/common.php
@@ -0,0 +1,7 @@
+<?php
+// 应用公共文件
+
+function get_max_user_no()
+{
+
+}
---
+++ app/controller/Index.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\controller;
+
+use app\BaseController;
+use app\controller\RandChinaName;
+
+class Index extends BaseController
+{
+    public function index()
+    {
+        $rcn_ctr = new RandChinaName();
+        return $rcn_ctr->getName(0);
+    }
+
+    public function hello($name = 'ThinkPHP6')
+    {
+        return 'hello,' . $name;
+    }
+}
